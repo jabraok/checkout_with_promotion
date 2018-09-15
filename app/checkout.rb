6ea135517a089cb65370147147108219780e263e
Vinh Luu <vinhlt3@gmail.com>
2018-09-15 10:55:18 +0700

    Add item model and checkout class

---
+++ app/checkout.rb
@@ -0,0 +1,29 @@
+class Checkout
+  attr_reader :promotional_rules
+
+  def initialize(promotional_rules)
+    @promotional_rules = promotional_rules
+
+    # Item array of hash: { item: item, quantity: 2 }
+    @basket_item_list = []
+  end
+
+  def scan(item)
+    existing_item = find_by_code(item.code)
+    if existing_item
+      existing_item[:quantity] += 1
+    else
+      @basket_item_list << { item: item, quantity: 1 }
+    end
+  end
+
+  def total
+    # @TODO: implement this method
+  end
+
+  private
+
+  def find_by_code(code)
+    @basket_item_list.detect { |basket_item| basket_item[:item].code == code }
+  end
+end
