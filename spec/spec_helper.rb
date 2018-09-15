07b0e99b8b963176a582406962f48c4cae20b513
Vinh Luu <vinhlt3@gmail.com>
2018-09-15 10:39:09 +0700

    Init first test case of promotional rule on total

---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+require 'pry'
+
+ordered_directories = %w[libs helpers models **]
+ordered_directories.each do |directory|
+  Dir[Dir.pwd + "/app/#{directory}/*.rb"].each { |file| require file }
+end
